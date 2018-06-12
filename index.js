0c2bb22fbfb665f0ddf7d6cb1642ff211187fc39
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-06-12 21:39:04 +0000

    client.login(process.env.TOKEN);

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const client = new Discord.Client();
 var prefix = "_";
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "adoumynoob")) {
+            if (message.content.startsWith(prefix + "mommy")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
  m.send(`${argresult}\n ${m}`);
 })
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Number of receiving members`); 
  message.delete(); 
 };     
 });
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : @HiMoxY™#1027  ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`_test | HiMoxY`,"https://www.twitch.tv/himoxytm")
+client.user.setGame(`developed by | @HiMoxY™#1027`,)
 client.user.setStatus("dnd")
 });
 
