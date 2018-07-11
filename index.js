6672e57908912e17a5c28df22bcec8102eee83b2
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-07-11 16:01:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,175 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "_";
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "mommy")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Number of receiving members`); 
- message.delete(); 
-};     
-});
-
-
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : @HiMoxY™#1027`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : @HiMoxY™#1027  ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`developed by | @HiMoxY™#1027`,)
-client.user.setStatus("dnd")
+  client.user.setGame(`.help | By @HiMoxY™#1027 .`,'');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
+client.on('message', message => {
+     if (message.content === "bg servers") {
+     let embed = new Discord.RichEmbed()
+  .setColor("#0000FF")
+  .addField("**Server: **" , client.guilds.size)
+  message.channel.sendEmbed(embed);
+    }
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+if (message.content.startsWith(prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **bg invite** ' ,' **Add bot to server** ')
+.addField('     **_server**  ' ,' **information about The server** ')
+.addField('     **bg support** ' , '**Server support**') 
+.addField('     **_cats** ' , '**send pictures of cats**') 
+.addField('     **bg avatar** ' ,' ** free access to personnel account picture**')
+.addField('     **bg id** ' , '**your profile id**')
+.setColor('#7d2dbe')
+.setFooter("Breakfast Gaming™")
+.setTimestamp() 
+  message.channel.sendEmbed(embed);
+    }
+});
+client.on('message', message => {
+  if(message.content === ('clear')) {
+  let modRole = message.guild.roles.find("name", "Admin");
+  if (!modRole) return message.reply('You do not have Admin Role'); {
+    }
+  const params = message.content.split(" ").slice(1)
+    let messagecount = parseInt(params[0]);
+    message.channel.fetchMessages({limit: messagecount})
+        .then(messages => message.channel.bulkDelete(messages));
+  }
+});
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+      if(!message.member.hasPermission('ADMINISTRATOR'))
+        message.delete()
+    return message.reply(`** No Invite Links :angry: ! **`)
+    }
+	if (message.content === 'bg play') {
+    if (message.member.voiceChannel) {       
+        message.member.voiceChannel.join()
+        .then(connection => {
+        connection.playStream("http://19763.live.streamtheworld.com/977_HITS.mp3"); //for playing an audiostream/radio
+        })
+        .catch(console.log);
+        message.reply('Joined and started playing.');
+    } else {
+                message.reply('You first need to join a voicechannel!');
+     }
+}
+});
+client.on('message', msg => {
+  if (msg.content === 'bg invite') {
+    msg.reply('https://discordapp.com/oauth2/authorize/?permissions=805314622&scope=bot&client_id=466612619223236638');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'bg help') {
+    msg.reply(':envelope: | Message sent in private');
+  }
+});
+client.on("message", message => {
+    var prefix = "bg";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **you dont have permission**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done ",
+        color: 0x06DF00,
+        description: "Messages successfully cleared",
+        footer: {
+          text: "Breakfast Gaming."
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
 
+     
+});
+// Your Avatar URL!
+client.on('message', message => {
+    if (message.content === "bg Avatar") {
+    message.reply(message.author.avatarURL); 
+    }
+});
+client.on('message', msg => {
+  if (msg.content === 'bg suppport') {
+    msg.reply('server support join ,https://discord.gg/ndpeSCU');
+  }
+});
+client.on("message", message => {
+      if (message.content === "bg ping") {
+      const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField('**Ping:**' , `${Date.now() - message.createdTimestamp}` + ' ms')
+  message.channel.sendEmbed(embed);
+    }
+});
+    client.on('message', message => {
+     if (message.content === "bg id") {
+     let embed = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)  
+  .setAuthor(message.author.username)
+.setDescription("Account information")
+               .setFooter(`HiMoxY™🔊.`, '')
+  .setColor("#9B59B6")
+  .addField("account name", `${message.author.username}`)
+  .addField('account code', message.author.discriminator)
+  .addField("YOUR ID", message.author.id)
+  .addField('Bots', message.author.bot)
+  .addField("date registration", message.author.createdAt)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+var prefix = ".";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+client.on("guildMemberRemove", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage("", {embed: {
+  color: 808080,
+  author: {
+    name: member.user.username,
+    icon_url: member.user.avatarURL
+  },
+  title: guild.name,
+  description: ' Bye ..',
+}}).catch(console.error);
+  }
+);
+client.on("message", (message) => {
+    if (message.content.startsWith("bg ban ")) {
+      if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply(' you dont have permission');
+        var member= message.mentions.members.first();
+        member.ban().then((member) => {
+            message.channel.send(member.displayName + " He was successfully expelled :wave: ");
+        }).catch(() => {
+            message.channel.send(":x: There are mistakes try again:x: ");
+        });
+    }
+});
 client.login(process.env.TOKEN);
